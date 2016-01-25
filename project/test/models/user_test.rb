97bfdf2e74553cba8b9993ba21f00da634d60616
AndreasBom <andreas.bom@gmail.com>
2016-01-25 13:16:58 +0100

    fixed test

--- project/test/models/user_test.rb
+++ project/test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  def setup
+  test "setup" do
     @user = User.new(first_name: "bob", last_name: "bobbsson", email: "anan@an.na")
   end
 end
