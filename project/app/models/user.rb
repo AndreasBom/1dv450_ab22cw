173d424c7bd154e50e581614ec28628bbbdbea4f
AndreasBom <andreas.bom@gmail.com>
2016-01-25 19:31:20 +0100

    userModel done

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  has_secure_password
+
   has_many :apis
   has_and_belongs_to_many :roles
-
-
-
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, email: true
   validates :first_name, :last_name, length: {maximum: 50}
   validates :email, uniqueness: true
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
 
 end
--- project/test/models/user_test.rb
+++ project/test/models/user_test.rb
@@ -2,7 +2,21 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "setup" do
-    @user = User.new(first_name: "bob", last_name: "bobbsson", email: "anan@an.na")
+  def setup
+    @user = User.create(first_name: "bob", last_name: "bobbsson", email: "anannn@an.na",
+    password: "fooBar", password_confirmation: "fooBar" )
+
   end
+
+  test "saving user" do
+    assert_not_nil(@user)
+  end
+
+  test "Testing uniqueness of email" do
+    user2 = User.create(first_name: "bob", last_name: "bobbsson", email: "anannn@an.na",
+    password: "fooBar", password_confirmation: "fooBar" )
+
+    assert(user2)
+  end
+
 end
